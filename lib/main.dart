dda2116c465e451bbe5fd94bb24965f56453bd4c
Sung Yan Micah Chao <symchao@gmail.com>
2020-04-04 22:45:12 -1000

    create a function that will handle the construction of a diamond image based on the function argument.

--- lib/main.dart
+++ lib/main.dart
@@ -17,48 +17,37 @@ void main() => runApp(MaterialApp(
               mainAxisAlignment: MainAxisAlignment.center,
               crossAxisAlignment: CrossAxisAlignment.center,
               children: <Widget>[
-                Image(
-                  image: AssetImage('images/diamond.png'),
-                  height: 100.0,
-                  width: 100.0,
-                ),
-                Image(
-                  image: AssetImage('images/diamond.png'),
-                  height: 100.0,
-                  width: 100.0,
-                ),
-                Image(
-                  image: AssetImage('images/diamond.png'),
-                  height: 100.0,
-                  width: 100.0,
-                ),
+                createDiamondImage(100.0),
+                createDiamondImage(100.0),
+                createDiamondImage(100.0),
               ],
             ),
-            Image(
-              image: AssetImage('images/diamond.png'),
-            ),
+            createDiamondImage(null),
             Row(
               mainAxisAlignment: MainAxisAlignment.center,
               crossAxisAlignment: CrossAxisAlignment.center,
               children: <Widget>[
-                Image(
-                  image: AssetImage('images/diamond.png'),
-                  height: 100.0,
-                  width: 100.0,
-                ),
-                Image(
-                  image: AssetImage('images/diamond.png'),
-                  height: 100.0,
-                  width: 100.0,
-                ),
-                Image(
-                  image: AssetImage('images/diamond.png'),
-                  height: 100.0,
-                  width: 100.0,
-                ),
+                createDiamondImage(100.0),
+                createDiamondImage(100.0),
+                createDiamondImage(100.0),
               ],
             ), //Icon(Icons.airport_shuttle, size: 36.0),
           ],
         ),
       ),
     ));
+
+// create a diamond image based on the argument's size.
+// if the argument is null, return a full size diamond
+createDiamondImage(imageSize) {
+  if (imageSize == null) {
+    return Image(
+      image: AssetImage('images/diamond.png'),
+    );
+  }
+  return Image(
+    image: AssetImage('images/diamond.png'),
+    height: imageSize,
+    width: imageSize,
+  );
+}
